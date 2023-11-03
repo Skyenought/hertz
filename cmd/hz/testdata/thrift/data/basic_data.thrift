b74fc244a60c97530d81b195b06405b5e8c99b7a
Skyenought <skyenought@qq.com>
2023-11-03 19:39:55 +0800

    update benchmark setting

--- cmd/hz/testdata/thrift/data/basic_data.thrift
+++
@@ -1,5 +0,0 @@
-namespace go toutiao.middleware.hertz_data
-
-struct BasicDataType {
-    1: optional string IsBasicDataString;
-}
--- cmd/hz/testdata/thrift/data/data.thrift
+++
@@ -1,7 +0,0 @@
-include "basic_data.thrift"
-
-namespace go toutiao.middleware.hertz_data
-
-struct DataType {
-    1: optional basic_data.BasicDataType IsDataString;
-}
